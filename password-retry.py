9cb45399e94b88bb709534219d1d675002cad98e
Andy Huang <andy2244759@gmail.com>
2022-03-16 22:58:40 +0800

    No zero times version

--- password-retry.py
+++ password-retry.py
@@ -1,12 +1,14 @@
 password = "a123456"
 i = 0
-while True:
+while i < 3:
+    i = i + 1
     pwd = input("請輸入密碼: ")
     if pwd == password:
         print("登入成功!")
         break
     else:
-        i = i + 1
-        print("密碼錯誤! 還有", 3 - i , "次機會!")
-        if i == 3:
-            break
+        print("密碼錯誤!")
+        if i < 3:
+            print("還有", 3 - i , "次機會!")
+        else:
+            print("沒機會嘗試了! 要鎖帳號了")
