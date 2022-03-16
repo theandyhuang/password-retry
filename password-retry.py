e3c0fe1040853576ae04e6822327af93327b6d75
Andy Huang <andy2244759@gmail.com>
2022-03-16 22:41:53 +0800

    basic version

---
+++ password-retry.py
@@ -0,0 +1,10 @@
+password = "a123456"
+i = 0
+while i < 3:
+    pwd = input("請輸入密碼: ")
+    if pwd == password:
+        print("登入成功!")
+        break
+    else:
+        i = i + 1
+        print("密碼錯誤! 還有", 3 - i , "次機會!")
